e5d445a52bba6d34a987aae98a9271dd4acb705b
MrGians <gianlumura97@gmail.com>
2022-09-15 17:49:26 +0200

    added Header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,26 @@
+<?php
+$header_links = config('data.header-links');
+?>
+
+<header>
+    <div class="container">
+      <!-- Main Navbar -->
+      <nav id="navbar">
+        <!-- DC Logo -->
+        <a href="{{ route('characters') }}">
+          <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo" />
+        </a>
+
+        <!-- Navbar Links -->
+        <ul>
+          @foreach ($header_links as $link)
+          <li>
+            <a href="{{ route(link['route_name']) }}" class="{{ request()->routeIs($link['route_name']) ? 'active' : '' }}">
+              {{ $link['text'] }}
+            </a>
+          </li>
+          @endforeach
+        </ul>
+      </nav>
+    </div>
+  </header>
