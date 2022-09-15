9fa6720798e50a8e5a57b52818a5fb7b5d18b6e6
MrGians <gianlumura97@gmail.com>
2022-09-15 17:49:38 +0200

    added Footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,94 @@
+<?php 
+$footer_links = config('data.footer-links');
+?>
+
+<footer>
+    <!-- Footer Top -->
+    <section id="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <!-- Footer Top Links -->
+            <div class="row links">
+              <div class="col">
+                <!-- DC Comics List -->
+                <div id="dc-comics">
+                  <h4>DC Comics</h4>
+                  <ul>
+                    @foreach ($footer_links['footer_dc_comics_links'] as $link)
+                    <li>
+                      <a href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+                    </li>
+                    @endforeach
+                  </ul>
+                </div>
+                <!-- Shop List -->
+                <div id="shop">
+                  <h4>Shop</h4>
+                  <ul>
+                    @foreach ($footer_links['footer_shop_links'] as $link)
+                    <li>
+                      <a href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+                    </li>
+                    @endforeach
+                  </ul>
+                </div>
+              </div>
+              <div class="col">
+                <!-- DC -->
+                <div id="dc">
+                  <h4>DC</h4>
+                  <ul>
+                    @foreach ($footer_links['footer_dc_links'] as $link)
+                    <li>
+                      <a href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+                    </li>
+                    @endforeach
+                  </ul>
+                </div>
+              </div>
+              <div class="col">
+                <!-- Sites -->
+                <div id="sites">
+                  <h4>Sites</h4>
+                  <ul>
+                    @foreach ($footer_links['footer_sites_links'] as $link)
+                    <li>
+                      <a href="{{ $link['url'] }}">{{ $link['text'] }}</a>
+                    </li>
+                    @endforeach
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col logo">
+            <!-- Footer Top Bg-logo -->
+            <img src="{{ asset('img/dc-logo-bg.png') }}" alt="DC Background Logo" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer Bottom -->
+    <section id="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <!-- SignUp Now Btn -->
+          <div class="col">
+            <a class="btn" href="#">Sign-up Now!</a>
+          </div>
+          <!-- Social Follow Links -->
+          <div class="col social-links">
+            <h4>Follow Us</h4>
+            @foreach ($footer_links['footer_social_links'] as $social)
+            <a href="#"><img src="{{ asset($social['src']) }}" alt="{{ $social['text'] }}"/></a>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+</footer>
