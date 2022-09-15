49c6204720572eef3b26f3b2bc3915251c797384
MrGians <gianlumura97@gmail.com>
2022-09-15 21:21:46 +0200

    added subpage [WIP]

--- routes/web.php
+++ routes/web.php
@@ -13,46 +13,63 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Homepage
+// Homepage (Characters)
 Route::get('/', function () {
-    $dc_series = config('data.dc-comic-series');
+    $comics = config('data.comics');
     $banner_items = config('data.banner-items');
-    return view('home', compact('dc_series', 'banner_items'));
+    return view('home', compact('comics', 'banner_items'));
 })->name('characters');
 
+    // | Subpage of Characters page
+    Route::get('/current-series', function () {
+        $comics = config('data.comics');
+        $banner_items = config('data.banner-items');
+        return view('current-series', compact('comics', 'banner_items'));
+    })->name('current-series');
 
+
+
+// Comics
 Route::get('/comics', function () {
     return view('comics');
 })->name('comics');
 
+// Movies
 Route::get('/movies', function () {
     return view('movies');
 })->name('movies');
 
+// TV
 Route::get('/tv', function () {
     return view('tv');
 })->name('tv');
 
+// Games
 Route::get('/games', function () {
     return view('games');
 })->name('games');
 
+// Collectibles
 Route::get('/collectibles', function () {
     return view('collectibles');
 })->name('collectibles');
 
+// Videos
 Route::get('/videos', function () {
     return view('videos');
 })->name('videos');
 
+// Fans
 Route::get('/fans', function () {
     return view('fans');
 })->name('fans');
 
+// News
 Route::get('/news', function () {
     return view('news');
 })->name('news');
 
+// Shop
 Route::get('/shop', function () {
     return view('shop');
 })->name('shop');
