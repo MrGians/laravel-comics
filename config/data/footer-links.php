86e96b100a99c89cc206d4168304237aaf0c57e9
MrGians <gianlumura97@gmail.com>
2022-09-15 17:34:28 +0200

    fixed footer-data config files

---
+++ config/data/footer-links.php
@@ -0,0 +1,118 @@
+<?php
+return [
+  'footer_dc_comics_links' => [
+  [
+    'text' => "Characters",
+    'url' => "#",
+  ],
+  [
+    'text' => "Comics",
+    'url' => "#",
+  ],
+  [
+    'text' => "Movies",
+    'url' => "#",
+  ],
+  [
+    'text' => "TV",
+    'url' => "#",
+  ],
+  [
+    'text' => "Games",
+    'url' => "#",
+  ],
+  [
+    'text' => "Videos",
+    'url' => "#",
+  ],
+  [
+    'text' => "News",
+    'url' => "#",
+  ],
+],
+  'footer_dc_links' => [
+  [
+    'text' => "Terms Of Use",
+    'url' => "#",
+  ],
+  [
+    'text' => "Privacy policy (New)",
+    'url' => "#",
+  ],
+  [
+    'text' => "Ad Choices",
+    'url' => "#",
+  ],
+  [
+    'text' => "Advertising",
+    'url' => "#",
+  ],
+  [
+    'text' => "Jobs",
+    'url' => "#",
+  ],
+  [
+    'text' => "Subscriptions",
+    'url' => "#",
+  ],
+  [
+    'text' => "Talent Workshops",
+    'url' => "#",
+  ],
+  [
+    'text' => "CPSC Certificates",
+    'url' => "#",
+  ],
+  [
+    'text' => "Ratings",
+    'url' => "#",
+  ],
+  [
+    'text' => "Shop Help",
+    'url' => "#",
+  ],
+  [
+    'text' => "Contact Us",
+    'url' => "#",
+  ],
+],
+  'footer_shop_links' => [
+  [
+    'text' => "Shop DC",
+    'url' => "#",
+  ],
+  [
+    'text' => "Shop DC Collectibles",
+    'url' => "#",
+  ],
+  ],
+  'footer_sites_links' => [
+  [
+    'text' => "DC",
+    'url' => "#",
+  ],
+  [
+    'text' => "MAD Magazine",
+    'url' => "#",
+  ],
+  [
+    'text' => "DC Kids",
+    'url' => "#",
+  ],
+  [
+    'text' => "DC Universe",
+    'url' => "#",
+  ],
+  [
+    'text' => "DC Power Visa",
+    'url' => "#",
+  ],
+],
+  'footer_social_links' => [
+    [ 'text' => "Facebook", 'src' => "img/footer-facebook.png"],
+    [ 'text' => "Periscope", 'src' => "img/footer-periscope.png"],
+    [ 'text' => "Pinterest", 'src' => "img/footer-pinterest.png"],
+    [ 'text' => "Twitter", 'src' => "img/footer-twitter.png"],
+    [ 'text' => "Youtube", 'src' => "img/footer-youtube.png"],
+  ]
+];
--- config/data/footer-social-links.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-  [ 'text' => "Facebook", 'src' => "img/footer-facebook.png"],
-  [ 'text' => "Periscope", 'src' => "img/footer-periscope.png"],
-  [ 'text' => "Pinterest", 'src' => "img/footer-pinterest.png"],
-  [ 'text' => "Twitter", 'src' => "img/footer-twitter.png"],
-  [ 'text' => "Youtube", 'src' => "img/footer-youtube.png"],
-];
